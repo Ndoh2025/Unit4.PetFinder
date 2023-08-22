9d5d99de4b0c710b6c3862d0db3c587d17b71752
Ndoh2025 <ndohwilson@yahoo.com>
2023-08-21 19:53:42 -0500

    initial commit

--- index.js
+++ index.js
@@ -10,44 +10,47 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+res.json(pets);
 });
 
 // hello world route
 app.get('/api', (req, res) => {
+    // Find pets with the matching name and return them as JSO
     res.send('Hello World!');
 });
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+res.send(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
-
+    // get the owner from the request 
+    const owner = req.query.owner;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
-
+    const matchingPets = pets.filter(pet => pet.owner === owner);
     // send the pet as a response
-
+    res.send(matchingPets);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
+    const name = req.params.name;
+
     // get the name from the request
 
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const matchingPets = pets.filter(pet => pet.name === name);
+    res.send(matchingPets);
 
     // send the pet as a response
 
 });
-
+app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
 });
